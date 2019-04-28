e13c72ca4582236bccba85200c410f6920927d24
Anwar Abdullah <anwarabdullahn28@gmail.com>
2019-04-28 23:39:00 +0700

    change index.js to app.js

--- middleware/Authorized.js
+++ middleware/Authorized.js
@@ -1,14 +1,13 @@
-const User = require('../models/User'),
-	authorized = (req, res, next) => {
-		User.findById(req.user._id).then(
-			(user) =>
-				user.deletedAt
-					? res.status(400).json({
-							success: false,
-							msg: 'Your Account Blocked'
-						})
-					: next()
-		);
-	};
+const User = require('../models/User');
 
-module.exports = authorized;
+module.exports = (req, res, next) => {
+	User.findById(req.user._id).then(
+		(user) =>
+			user.deletedAt
+				? res.status(400).json({
+						success: false,
+						msg: 'Your Account Blocked'
+					})
+				: next()
+	);
+};
--- middleware/checkBalance.js
+++ middleware/checkBalance.js
@@ -1,11 +1,14 @@
-const updateUserWallet = (req, res, next) => {
+const User = require('../models/User'),
+	Wallet = require('../models/Wallet');
+
+module.exports = (req, res, next) => {
 	User.findById(req.user._id)
 		.then((user, err) => {
 			err && res.status(400).json(err);
 			Wallet.find({ userId: req.user._id })
 				.then((wallet, err) => {
-                    err && res.status(400).json(err);
-                    user.balance = 0
+					err && res.status(400).json(err);
+					user.balance = 0;
 					for (let i = 0; i < wallet.length; i++) user.balance += wallet[i].balance;
 					user.save().then(
 						(user) =>
@@ -21,6 +24,3 @@ const updateUserWallet = (req, res, next) => {
 		})
 		.catch((err) => res.status(400).json(err));
 };
-
-
-module.exports = updateUserWallet;
